902ec431b0227d3da58a21a44f29825dad96a601
Nazareno Valentini <nazareno.valentini@gmail.com>
2022-07-25 17:13:49 +0200

    add Escena component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Escena from './components/escena/Escena';
+
 
 function App() {
+
+  const text = "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial";
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Escena testo={text} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/escena/Escena.js
@@ -0,0 +1,10 @@
+import "./Escena.css"
+
+const Escena = (props) => {
+
+    return (
+        <p>{props.testo}</p>
+    )
+};
+
+export default Escena;
